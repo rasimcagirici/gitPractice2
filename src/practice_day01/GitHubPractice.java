07d38d2d867e92249f822374942dba41f2c73653
Rasim Cagirici <rasimcagirici@gmail.com>
2021-04-27 20:36:35 -0400

    Making initial commit

---
+++ src/practice_day01/GitHubPractice.java
@@ -0,0 +1,7 @@
+package practice_day01;
+
+public class GitHubPractice {
+    public static void main(String[] args) {
+        System.out.println("Practice to see if it will show on GitHub");
+    }
+}
