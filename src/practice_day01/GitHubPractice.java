1fc424fb44ea9645df2e34f0b9e78dbd6118a8a9
Rasim Cagirici <rasimcagirici@gmail.com>
2021-04-27 20:51:13 -0400

    Making second commit

--- src/practice_day01/GitHubPractice.java
+++ src/practice_day01/GitHubPractice.java
@@ -3,5 +3,6 @@ package practice_day01;
 public class GitHubPractice {
     public static void main(String[] args) {
         System.out.println("Practice to see if it will show on GitHub");
+        System.out.println("Second commit for practice");
     }
 }
